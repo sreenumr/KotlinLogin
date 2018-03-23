0e2ac12af7ceea29491e9a24d738c0415f1d3264
sreenumr <sreenu959@gmail.com>
2018-03-23 11:58:00 +0530

    Feat: Shows User details in Database

--- app/src/main/java/com/example/sreenu/login/MainActivity.kt
+++ app/src/main/java/com/example/sreenu/login/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.sreenu.login
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+private  var email:String? = null
+private var passsword:String? = null
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        signUpText.setOnClickListener {
+            finish()
+            val signUpPage = Intent(this,Registration::class.java)
+
+            startActivity(signUpPage)
+
+        }
+
+
+
     }
 }
