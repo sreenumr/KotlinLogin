bf8f505c7179179b7366bc7f90a5c93cb3acbd06
sreenumr <sreenu959@gmail.com>
2018-03-23 09:07:39 +0530

    Feat: Declared Functions for User Registration

--- app/src/main/java/com/example/sreenu/login/Registration.kt
+++ app/src/main/java/com/example/sreenu/login/Registration.kt
@@ -4,8 +4,12 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_registration.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.auth.FirebaseAuth
@@ -25,12 +29,12 @@ class Registration : AppCompatActivity() {
 
     private var etFirstName: EditText? = null
     private var etLastName: EditText? = null
-    private var userName: EditText? = null
+    private var etUserName: EditText? = null
     private var etEmail: EditText? = null
     private var etPhoneNumber: EditText? = null
     private var etPassword: EditText? = null
     private var btnRegister: Button? = null
-    private var mProgressBar: ProgressDialog? = null
+    private var mProgressBar: ProgressBar? = null
 
     //Firebase Refs
     private var mDatabaseReference: DatabaseReference? = null
@@ -48,7 +52,43 @@ class Registration : AppCompatActivity() {
 
         }
 
+        initialise()
 
+    }
 
+    private fun initialise(){
+
+        etFirstName = findViewById<View>(R.id.regFirstName) as EditText
+        etLastName  = findViewById<View>(R.id.regLastName) as EditText
+        etUserName  = findViewById<View>(R.id.regUserName) as EditText
+        etEmail  = findViewById<View>(R.id.regEmail) as EditText
+        etPhoneNumber  = findViewById<View>(R.id.regPhoneNumber) as EditText
+        etPassword = findViewById<View>(R.id.regPassword) as EditText
+        btnRegister = findViewById<View>(R.id.btnSignUp) as Button
+
+        mProgressBar = ProgressBar(this)
+        mDatabase = FirebaseDatabase.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+        btnRegister!!.setOnClickListener { createNewAccount() }
+    }
+
+    private fun createNewAccount(){
+        firstName = etFirstName!!.text.toString()
+        lastName = etLastName!!.text.toString()
+        userName = etUserName!!.text.toString()
+        email = etEmail!!.text.toString()
+        phoneNumber = etPhoneNumber!!.text.toString()
+        password = etPassword!!.text.toString()
+
+        if(!TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName)
+                && !TextUtils.isEmpty(userName) && !TextUtils.isEmpty(email)
+                && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(phoneNumber))
+
+                Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show();
+
+            else {
+
+            Toast.makeText(this,"Fields are Empty!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
