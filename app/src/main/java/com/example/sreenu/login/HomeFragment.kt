ebb0ac51ae6b15745b501b8f00f9d9f78fcfbd70
sreenumr <sreenu959@gmail.com>
2018-04-20 16:28:40 +0530

    Feat: added mapview

--- app/src/main/java/com/example/sreenu/login/HomeFragment.kt
+++ app/src/main/java/com/example/sreenu/login/HomeFragment.kt
@@ -5,13 +5,77 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.OnMapReadyCallback
+
+
+
+
 
 class HomeFragment:Fragment(){
 
-//    var mMapView: MapView? = null
-//    private val googleMap: GoogleMap? = null
+      var mMapView: MapView? = null
+      private var googleMap: GoogleMap? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home,null)
+
+        val myInflatedMapView =  inflater.inflate(R.layout.fragment_home,null)
+
+
+        mMapView = myInflatedMapView.findViewById(R.id.mapView)
+        mMapView!!.onCreate(savedInstanceState)
+
+        mMapView!!.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(activity!!.applicationContext)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        mMapView!!.getMapAsync(OnMapReadyCallback { mMap ->
+            googleMap = mMap
+
+            // For showing a move to my location button
+
+
+           // googleMap!!.isMyLocationEnabled = true
+
+            // For dropping a marker at a point on the Map
+            val amrita  = LatLng(9.0936997, 76.49149549999993)
+            googleMap!!.addMarker(MarkerOptions().position(amrita).title("Marker Title").snippet("Marker Description"))
+
+            // For zooming automatically to the location of the marker
+            val cameraPosition = CameraPosition.Builder().target(amrita).zoom(12f).build()
+            googleMap!!.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        })
+
+        return myInflatedMapView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView!!.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView!!.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView!!.onLowMemory()
     }
 }
