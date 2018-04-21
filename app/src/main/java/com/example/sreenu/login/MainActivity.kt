6fa66c4f3c4811e388aaf52a9cc64d9aaee4f284
sreenumr <sreenu959@gmail.com>
2018-04-21 08:51:38 +0530

    Feat:Single instance login and registration activity

--- app/src/main/java/com/example/sreenu/login/MainActivity.kt
+++ app/src/main/java/com/example/sreenu/login/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sreenu.login
 
 import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,19 +11,30 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.basgeekball.awesomevalidation.AwesomeValidation
-import com.google.android.gms.tasks.Task
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.net.NetworkInfo
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.support.v4.view.GravityCompat
+import android.view.Gravity
+import kotlinx.android.synthetic.main.activity_profile_main.*
+
 
 private  var email:String? = null
 private var password:String? = null
+private  var toast:Toast?=null
 
 class MainActivity : AppCompatActivity() {
 
+    private var rootRef = FirebaseDatabase.getInstance().getReference()
+    private var query :Query?=null
 
     private val TAG = "Login"
 
+    private var loginUserName:String? = null
     private var etEmail:EditText? = null
     private var etPassword:EditText? = null
     private var loginButton:Button? = null
@@ -51,7 +63,18 @@ class MainActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
-        loginButton!!.setOnClickListener { loginUser() }
+        loginButton!!.setOnClickListener {
+
+            if(isOnline())
+            loginUser()
+
+            else {
+                toast =  Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT)
+                toast!!.setGravity(Gravity.TOP,0,0)
+                toast!!.show()
+            }
+
+            }
     }
 
     private fun loginUser(){
@@ -65,6 +88,7 @@ class MainActivity : AppCompatActivity() {
             mProgressBar!!.show()
 
             Log.d(TAG,"Logging in user")
+
 
             mAuth!!.signInWithEmailAndPassword(email!!, password!!)
                     .addOnCompleteListener(this){task ->
@@ -88,6 +112,24 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, profile_main::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
+    }
+
+    private fun isOnline(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = cm.activeNetworkInfo
+         if (netInfo != null && netInfo.isConnectedOrConnecting)
+           return  true
+         else
+            return false
+
+    }
+
+    private fun checkConnection() {
+        if (isOnline()) {
+            Toast.makeText(this, "You are connected to Internet", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "You are not connected to Internet", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
